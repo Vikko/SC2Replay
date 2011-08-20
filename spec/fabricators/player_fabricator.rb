9a9b8896fb744c2dcd3b2ed35378eca3cebd6f2a
Vikko Smit <vikkosmi@hotmail.com>
2011-08-20 13:27:20 +0200

    Fix

---
+++ spec/fabricators/player_fabricator.rb
@@ -0,0 +1,5 @@
+Fabricator :player do
+  name {"Vikko"}
+  race {1}
+  team {0}
+end
---
+++ spec/models/player_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Player do
+  it{ should have(0).errors_on(:name) }
+  it{ should have(0).errors_on(:race) }
+  it{ should have(0).errors_on(:team) }
+  
+  context "After object creation" do
+    subject { Fabricate(:player) }
+   
+    it {should be_valid}
+  end
+end
+
