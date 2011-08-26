923094a7092a5b7bb526b740634e5fc5142bdbe7
Vikko Smit <vikkosmi@hotmail.com>
2011-08-26 15:26:52 +0200

    New layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:index, :show]
   protect_from_forgery
 end
